a45e1eabaf353da6024e5f3c7ad578a52d8f11a9
saputt29 <sauki666indo@gmail.com>
2025-10-07 15:27:45 +0700

    mengerjakan fitur header - belum beres

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -5,7 +5,7 @@ const combineClass = ((baseClass, customClass) => {
 })
 
 const Input = ({className, onChange = (() => ''), placeholder}) => {
-    const baseClass = 'px-4 py-2 focus:outline-none border-[0.1px]';
+    const baseClass = 'px-4 py-2 focus:outline-none border-[0.1px] w-full';
 
     const finalClass = combineClass(baseClass, className);
 
