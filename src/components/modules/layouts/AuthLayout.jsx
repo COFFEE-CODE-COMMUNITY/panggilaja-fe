a6a158e3e04c618832dc785f141ef35e6c8bf72f
saputt29 <sauki666indo@gmail.com>
2025-10-08 10:47:47 +0700

    form lgin selesai dan register on progress

--- src/components/modules/layouts/AuthLayout.jsx
+++ src/components/modules/layouts/AuthLayout.jsx
@@ -5,28 +5,27 @@ import Button from '../../common/Button'
 
 const AuthLayout = ({title, subtitle, children, type, navAuth}) => {
   return (
-    <div className='flex min-h-screen'>
-
-      <div className='w-1/2'>
-        <div className='xl:p-[10px] md:p-[5px]'>
-          <NavLink link='/' className='text-primary text-h2 font-bold' text='PanggilAja'/>
+    <div className='flex h-screen relative'>
+      <div className='w-full md:w-1/2 h-full'>
+        <div className='xl:p-[10px] md:p-[5px] pl-[10px] sticky top-0 left-0 right-0 bg-white'>
+          <NavLink link='/' className='text-primary text-h3 lg:text-h2 font-bold' text='PanggilAja'/>
         </div>
-        <div className='flex justify-center items-center'>
-          <div className='px-[30px] py-[20px] bg-amber-200 w-[532px]'>
+        <div className='flex justify-center items-center h-full'>
+          <div className='px-[30px] py-[20px] w-[532px] flex flex-col gap-[10px]'>
             <div>
-              <h2 className='text-h2 font-medium'>{title}</h2>
-              <p className='text-secondary text-h4'>{subtitle}</p>
+              <h2 className='text-h3 lg:text-h2 font-medium'>{title}</h2>
+              <p className='text-h5 lg:text-secondary text-h4'>{subtitle}</p>
             </div>
-            <Button className='w-full h-[62px] bg-white rounded-[30px] text-h4'>Lanjut dengan google</Button>
-            <p className='w-full text-center'>atau</p>
+            <Button className='w-full h-[62px] bg-white rounded-[30px] text-h5 lg:text-h4'>Lanjut dengan google</Button>
+            <p className='w-full text-center text-h6 lg:text-h5 font-light'>atau</p>
             {children}
-            <Button className='w-full h-[62px] text-center text-h3 font-semibold bg-primary text-white rounded-[15px]'>{type}</Button>
+            <Button className='w-full h-[62px] text-center text-h4 lg:text-h3 font-semibold bg-primary text-white rounded-[15px]'>{type}</Button>
             {navAuth}
           </div>
         </div>
       </div>
 
-      <div className='w-1/2 '>
+      <div className='hidden md:block fixed top-0 bottom-0 right-0 md:w-1/2'>
         <div className='rounded-tl-[20px] rounded-bl-[20px] bg-amber-100 h-full w-full'></div>
       </div>
     </div>
--- src/page/Login/Index.jsx
+++ src/page/Login/Index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AuthLayout from '../../components/modules/layouts/AuthLayout'
 import NavLink from '../../components/modules/navigation/NavLink'
+import LoginForm from './sections/LoginForm'
 
 const LoginPage = () => {
   return (
@@ -9,12 +10,12 @@ const LoginPage = () => {
       subtitle='Masuk & Dapatkan Jasa Terbaik di Dekat Anda'
       type='Masuk'
       navAuth={
-        <p className='text-h5 text-primary w-full text-center'>
+        <p className='text-h6 lg:text-h5 text-primary w-full text-center'>
           Tidak punya akun? <NavLink className="font-semibold text-primary" link='/register' text='buat disini'/>'
         </p>
       }
     >
-      hai
+      <LoginForm/>
     </AuthLayout>
   )
 }
