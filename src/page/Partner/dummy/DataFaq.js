fa6160f14dc9f74fa86ab1853dfd1782e9874a7d
GeneralDwoy <kusmandana3547@gmail.com>
2025-10-29 00:23:23 +0700

    perubahan Faq

--- src/page/Partner/dummy/DataFaq.js
+++ src/page/Partner/dummy/DataFaq.js
@@ -1,31 +1,31 @@
 export const dataFaq = [
     {
         id: 1,
-        question: "Bagaimana cara aplikasi ini mendapatkan keuntungan/monetisasi?",
-        answer: "lorem ipsum.",
+        question: "Apa saja keuntungan bagi mitra menggunakan aplikasi ini?",
+        answer: "Jangkauan luas, mudah kelola pesanan, bangun reputasi, fleksibel tentukan harga & jam kerja.",
     },
 
     {
         id: 2,
         question: "Berapa biaya untuk mendaftar dan menjadi mitra PanggilAja?", 
-        answer: "lorem ipsum",
+        answer: "Santai aja, gratis kok! Buat gabung jadi mitra PanggilAja, kamu nggak perlu bayar apa-apa buat daftarnya.",
     },
 
     {
         id: 3,
         question: "Apakah saya bisa menentukan sendiri harga dan jam kerja saya?",
-        answer: "lorem ipsum.",
+        answer: "Tentu bisa! Kamu punya kontrol penuh untuk menentukan sendiri harga setiap jasamu dan jadwal kerja kapan kamu bersedia menerima panggilan. PanggilAja memberi kamu fleksibilitas penuh.",
     },
 
     {
         id: 4,
-        question: "Bagaimana jika konsumen memberikan review palsu atau negatif?",
-        answer: "lorem ipsum",
+        question: "Bagaimana cara mitra bisa mengiklankan jasanya di aplikasi ini?",
+        answer: "Mitra bisa membayar untuk promosi agar jasa atau profil mereka tampil di posisi teratas hasil pencarian atau di bagian khusus aplikasi, jadi lebih gampang dilihat pelanggan.",
     },
     
     {
         id: 5,
         question: "Bagaimana saya tahu kalau ada permintaan pekerjaan masuk?",
-        answer: "lorem ipsum",
+        answer: "Kamu akan mendapatkan notifikasi pesanan langsung di aplikasi PanggilAja setiap kali ada permintaan pekerjaan baru yang masuk untukmu.",
     },
 ];
