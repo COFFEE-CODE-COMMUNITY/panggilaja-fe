cd8ce1efe2e8cb283e69f4c4a8a08f8aa5262393
Muhammad Ryan Handhika Susanto <muhammadryanhandhikasusantorya@gmail.com>
2025-11-08 22:55:09 +0700

    Update ScrollToTop.jsx

--- src/components/utils/ScrollToTop.jsx
+++ src/components/utils/ScrollToTop.jsx
@@ -1,20 +1,14 @@
 import { useEffect } from 'react';
-import { useLocation, Outlet } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
-/**
- * Komponen yang memastikan halaman selalu scroll ke posisi paling atas (0, 0)
- * setiap kali terjadi perubahan route (URL).
- */
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    // Scroll ke posisi (0, 0) setiap kali 'pathname' berubah
-    window.scrollTo(0, 0); 
+    window.scrollTo(0, 0);
   }, [pathname]);
 
-  // Merender children/nested routes melalui Outlet
-  return <Outlet />;
+  return null;
 };
 
 export default ScrollToTop;
